9b3d4c2fcc3f000d92c77b3ad72a690b9ace6ba6
Vladymyr Danchevskyi <elvisco16@gmail.com>
2021-11-10 17:55:34 +0200

    end

--- js/task-08.js
+++ js/task-08.js
@@ -3,3 +3,26 @@
 // Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const loginForm = document.querySelector(".login-form");
+
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
+loginForm.addEventListener("submit", validation);
+
+function validation(event) {
+  event.preventDefault();
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+  if (email == "" || password == "") {
+    alert("Не все поля заполнены!");
+  }
+  alert("Данные отправлены!");
+  const user = new User(email, password);
+  console.log("user : ", user);
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+
+changeColorBtnEl.addEventListener("click", changePageColor);
+
+function changePageColor() {
+  const colorNum = getRandomHexColor();
+  bodyEl.setAttribute("style", `background-color:${colorNum}`);
+  viewColorNumber(colorNum);
+}
+function viewColorNumber(color) {
+  spanColor.textContent = color;
+}
